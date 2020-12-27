49d0373baf1fb6fa9cd8ed604bcdd09bb210b958
Firnando <firnandorfa@gmail.com>
2020-12-27 14:59:56 +0700

    update api

--- app/src/main/java/com/appsih/app/data/remote/RetrofitApi.kt
+++ app/src/main/java/com/appsih/app/data/remote/RetrofitApi.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitApi {
-    const val NEWS_URL = "https://www.news.developeridn.com/ekonomi/"
+    const val NEWS_URL = "https://www.news.developeridn.com/internasional/"
 
     private val client = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
